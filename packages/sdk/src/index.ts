04a76dbcec445e18b7cda229da7f3c0f7d0be9f7
moikapy <moikapy@devmoi.com>
2026-01-28 01:58:23 -0500

    zzz

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,7 +3,7 @@
  *
  * A high-performance CAD library for JavaScript/TypeScript with 98-99% OpenSCAD compatibility.
  *
- * @version 0.1.7
+ * @version 0.1.8
  * @author moicad
  * @license MIT
  *
@@ -225,7 +225,7 @@ export { initializePlugins as initializeFunctionalPlugins } from './functional';
 // VERSION AND METADATA
 // ============================================================================
 
-export const VERSION = '0.1.7';
+export const VERSION = '0.1.8';
 
 export function getInfo() {
   return {
